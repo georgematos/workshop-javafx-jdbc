43fa579c94458509dcaa34e39cbf847296fd7714
georgematos <georgemattos@gmail.com>
2020-01-01 17:17:25 -0300

    Update department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -38,6 +40,8 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	private TableColumn<Department, Integer> tableColumnId;
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
+	@FXML
+	private TableColumn<Department, Department> tableColumnEDIT;
 	@FXML
 	private Button btNew;
 
@@ -66,6 +70,26 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		this.depService = depService;
 	}
 
+	public void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Department department, boolean empty) {
+				super.updateItem(department, empty);
+				if (department == null) {
+					setGraphic(null);
+					return;
+				}
+
+				setGraphic(button);
+				button.setOnAction(event -> createDialogForm(department, depService, "/gui/DepartmentForm.fxml",
+						Utils.currentStage(event)));
+			}
+		});
+	}
+
 	public void updateTableView() {
 		if (depService == null) {
 			throw new IllegalStateException("Service was null");
@@ -73,6 +97,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		List<Department> list = depService.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+		initEditButtons();
 	}
 
 	private void createDialogForm(Department dep, DepartmentService service, String absolutePath, Stage parentStage) {
